c2dc9d1dddcf9747d72ac28fcb2fdf91590130e2
andresquevedodiaz <ja.quevedodiaz@gmail.com>
2020-05-26 01:22:18 -0500

    Ahora el script guarda no solo la ip pública, sino que acomapaña a esta con la fecha y hora.  Lo que nos permitirá llevar un registro de cambios de ip

--- ipublica.sh
+++ ipublica.sh
@@ -2,5 +2,6 @@
 #enviar mediante correo la ip publica
 echo escriba el correo al que desea enviar la ip
 read ip
-curl ifconfig.me > miip.txt
+curl=$(curl ifconfig.me)
+echo "[$(date)] $curl\n" | tee -a miip.txt
 sent=$(mail -s "ip pública" $ip < miip.txt)
