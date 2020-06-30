b4307bc6b403c4ff04a829b2a5a7ece74a0351ad
andresquevedodiaz <ja.quevedodiaz@gmail.com>
2020-06-30 18:03:21 -0500

    Esta oportunidad redefiní algunas variables y cambié sustancialmente la estructura del script

--- ex_001.sh
+++ ex_001.sh
@@ -1,84 +1,139 @@
 #!/bin/bash
-# Este script determina si el argumento es un fichero o un directorio
+# # Este script permite la busqueda y la caracterización de un fichero o directorio
 toilet -f mono12 -F metal -F border "find it"
 echo "                                                  by Andrés quevedo Díaz"
-# definición de colores
-color1="\e[1;7;37m"
-fincolor1="\e[0m"
-######## funciones ########
-# función de localización
+################################################## DEFINICIÓN DE COLORES
+
+color1="\e[1;7;35m"
+fincolor="\e[0m"
+
+color2="\e[1;7;34m"
+fincolor="\e[0m"
+
+################################################## FUNCIONES
+
+principal()
+{
+if [ -e $file ]; then
+        if [ -f $file ]; then
+                echo "$file es un fichero"
+                permisosr
+                permisosw
+                permisosx
+                localizarf
+        else
+                echo "$file es un directorio"
+                permisosr
+                permisosw
+                permisosx
+                localizard
+        fi
+else
+        echo "$file no existe"
+fi
+echo ""
+}
+
+############# función de permisos de lectura
+
+permisosr()
+{
+if [ -r $file ]; then
+        echo "Tiene permiso de lectura"
+else
+        echo "no tiene permiso de lectura"
+fi
+}
+
+############# función de permisos de escritura
+
+permisosw()
+{
+if [ -w $file ]; then
+        echo "Tiene permiso de escritura"
+else
+        echo "No tiene permiso de escritura"
+fi
+}
+
+############ función de permisos de ejecución
+
+permisosx()
+{
+if [ -x $file ]; then
+        echo "Tiene permiso de ejecución"
+else
+        echo "No tiene permiso de ejecución"
+fi
+}
+
+############ función de localización
+
+aqui=$(pwd)
 localizarf()
 {
-localf=$(find . -type f -iname $file* )
+localf=$(find $aqui -type f -iname $file )
 echo $localf
 }
 
 localizard()
 {
-locald=$(find . -type d -iname $file* )
+locald=$(find $aqui -type d -iname $file )
 echo $locald
 }
 
-# función de permisos de lectura
-permisosr()
+############ función continuar
+
+continuar()
 {
-if [ -r $file ]; then
-	echo "Tiene permiso de lectura"
+echo -n $color2"¿Desea continuar? (s/n) "$fincolor
+read continuar
+if [ $continuar = "s" ]; then
+        echo ""
+        menu
+
+elif [ $continuar = "n" ]; then
+        salir
 else
-	echo "no tiene permiso de lectura"
+        echo $continuar no es una opción
+        continuar
 fi
 }
-# función de permisos de escritura
-permisosw()
-{
-if [ -w $file ]; then
-	echo "Tiene permiso de escritura"
-else
-	echo "No tiene permiso de escritura"
-fi
-}
-# función de permisos de ejecución
-permisosx()
-{
-if [ -x $file ]; then
-	echo "Tiene permiso de ejecución"
-else
-	echo "No tiene permiso de ejecución"
-fi
-}
-#función salir
+
+############ función salir
+
 salir()
 {
 exit
 }
 
-# Función principal
-principal()
+############ función menu
+
+menu()
 {
-if [ -e $file ]; then
-	if [ -f $file ]; then
-		echo "$file es un fichero"
-		permisosr
-		permisosw
-		permisosx
-		localizarf
-	else
-		echo "$file es un directorio"
-		permisosr
-		permisosw
-		permisosx
-		localizard
-	fi
-else 
-	echo "$file no existe"
+echo $color2"      Parámetros de busqueda     "$fincolor
+read parametro
+if ls | grep $parametro; then
+        echo ""
+        explorar
+else
+        echo "No existen resultados"
+        echo ""
+        continuar
 fi
 }
 
-# menú principal
-echo -n $color1" Parámetros de busqueda "$fincolor1
+############ función explorar
+
+explorar()
+{
+echo $color2"Seleccione la opción a explorar: "$fincolor
 read file
-
 principal
+continuar
+}
 
+############ PRIMERA LLAMADA
 
+menu
 
